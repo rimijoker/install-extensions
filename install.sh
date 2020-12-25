ae61fd17ada77f28ac3356d2ca124b483720df10
rimijoker <anz19999999999@gmail.com>
2020-12-25 11:35:39 +0530

    initial commit

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/bash
+
+
+echo "Installing Gnome Extensions CLI"
+
+pip install gnome-extensions-cli
+
+echo "Updating Extensions"
+
+gnome-extensions-cli update
+
+echo "Installing Extensions"
+echo "Please Press Return when prompted :)"
+
+gnome-extensions-cli install caffeine@patapon.info 
+gnome-extensions-cli install compiz-alike-magic-lamp-effect@hermes83.github.com
+gnome-extensions-cli install compiz-alike-windows-effect@hermes83.github.com 
+gnome-extensions-cli install CoverflowAltTab@palatis.blogspot.com 
+gnome-extensions-cli install dash-to-dock@micxgx.gmail.com 
+gnome-extensions-cli install desktop-icons@csoriano 
+gnome-extensions-cli install drive-menu@gnome-shell-extensions.gcampax.github.com 
+gnome-extensions-cli install emoji-selector@maestroschan.fr 
+gnome-extensions-cli install gsconnect@andyholmes.github.io 
+gnome-extensions-cli install hidetopbar@mathieu.bidon.ca 
+gnome-extensions-cli install multi-monitors-add-on@spin83 
+gnome-extensions-cli install no-workspace-switcher-popup@devbury.com
+gnome-extensions-cli install refresh-wifi@kgshank.net 
+gnome-extensions-cli install update-extensions@franglais125.gmail.com
+gnome-extensions-cli install user-theme@gnome-shell-extensions.gcampax.github.com 
+gnome-extensions-cli install dynamic-panel-transparency@rockon999.github.io
+gnome-extensions-cli install openweather-extension@jenslody.de
+
+echo "Installed Extensions List"
+
+gnome-extensions-cli list
